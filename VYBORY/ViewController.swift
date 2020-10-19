9b10fc1c30e7df1dabe7fa2c774a7b59fb5a6f02
Oksana <klassi4eske@gmail.com>
2020-10-19 15:56:26 +0300

    Initial Commit

---
+++ VYBORY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VYBORY
+//
+//  Created by Oksana Fedorchuk on 19.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
