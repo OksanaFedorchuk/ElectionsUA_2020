285818e060aeccd5e6dd458be655dbc7a0e496cc
Oksana <klassi4eske@gmail.com>
2020-10-26 16:13:25 +0200

    two rows, but image and text go outside of the cell

--- VYBORY/Controllers/CodeViewController.swift
+++ VYBORY/Controllers/CodeViewController.swift
@@ -12,14 +12,15 @@ private let reuseIdentifier = "CollectionViewCell"
 class CodeViewController: UICollectionViewController {
     
     
-//    var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    //    var collectionViewFlowLayout: UICollectionViewFlowLayout!
     let db = Database()
+    let minimumInteritemSpacing: CGFloat = 10
+    
     var books = [String]()
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
         
     }
     // MARK: UICollectionViewDataSource
@@ -28,34 +29,36 @@ class CodeViewController: UICollectionViewController {
         // #warning Incomplete implementation, return the number of items
         return 4
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
         
-        var cell = UICollectionViewCell()
-        
-        if let bookCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell {
-//            let book = books[indexPath.row]
-//            let book = "Book"
-//            bookCell.configure(with: book)
+        let bookCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+            //            let book = books[indexPath.row]
+            //            let book = "Book"
+            //            bookCell.configure(with: book)
             bookCell.bookName.text = "Book Name"
             bookCell.bookNumber.text = "Book Number"
-        
-        bookCell.bookImage.image = UIImage(named: "01_book_01_dark")
             
-            cell = bookCell
-        
-        }
-        return cell
-    }
+            bookCell.bookImage.image = UIImage(named: "01_book_01_dark")
 
+        
+        return bookCell
+    }
+    
+    
+    
 }
 
-//extension CodeViewController: UICollectionViewDelegateFlowLayout {
-//    private func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-//
-////           return CGSize(width: 250, height: 250)
-//
-//        return CGSize(width: collectionView.bounds.size.width/2 - 10, height: collectionView.bounds.size.height/2 - 10)
-//    }
-//}
+extension CodeViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.bounds.size.width/2 - minimumInteritemSpacing,
+               height: collectionView.bounds.size.height/2 - minimumInteritemSpacing)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        minimumInteritemSpacing
+    }
+}
 
