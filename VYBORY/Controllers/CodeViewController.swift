e43cb8cf80085c2e725a5c2031f09fa9748f712a
Oksana <klassi4eske@gmail.com>
2020-10-26 15:20:53 +0200

    displays stack view with image and two labels in one row

--- VYBORY/Controllers/CodeViewController.swift
+++ VYBORY/Controllers/CodeViewController.swift
@@ -7,120 +7,55 @@
 
 import UIKit
 
-private let reuseIdentifier = "CodeBookCell"
+private let reuseIdentifier = "CollectionViewCell"
 
 class CodeViewController: UICollectionViewController {
     
-    var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    
+//    var collectionViewFlowLayout: UICollectionViewFlowLayout!
     let db = Database()
     var books = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Register cell classes
-        let nibCell = UINib(nibName: "CodeBookCell", bundle: nil)
-        self.collectionView!.register(nibCell, forCellWithReuseIdentifier: reuseIdentifier)
-        books = db.getBooks()
         
-//        setupCollectionViewItemSize()
-
-        // Do any additional setup after loading the view.
+//        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        
     }
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        setupCollectionViewItemSize()
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     // MARK: UICollectionViewDataSource
-
-//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-
-    private func setupCollectionViewItemSize() {
-        if collectionViewFlowLayout == nil {
-            let numberOfItemPerRow: CGFloat = 3
-            let lineSpacing: CGFloat = 5
-            let interIpemSpacing: CGFloat = 5
-
-            let width = (collectionView.frame.width - (numberOfItemPerRow - 1) * interIpemSpacing) / numberOfItemPerRow
-            let height = width
-
-            collectionViewFlowLayout = UICollectionViewFlowLayout()
-            collectionViewFlowLayout.itemSize = CGSize(width: width, height: height)
-            collectionViewFlowLayout.sectionInset = UIEdgeInsets.zero
-            collectionViewFlowLayout.minimumLineSpacing = lineSpacing
-            
-            collectionView.setCollectionViewLayout(collectionViewFlowLayout, animated: true)
-        }
-    }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         return 4
-//            db.showBooks()
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         var cell = UICollectionViewCell()
         
-        if let bookCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CodeBookCell {
+        if let bookCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell {
 //            let book = books[indexPath.row]
-            let book = "Book"
-            bookCell.configure(with: book)
-            bookCell.bookImage.image = UIImage(named: "01_book_01_dark")
+//            let book = "Book"
+//            bookCell.configure(with: book)
+            bookCell.bookName.text = "Book Name"
+            bookCell.bookNumber.text = "Book Number"
+        
+        bookCell.bookImage.image = UIImage(named: "01_book_01_dark")
             
             cell = bookCell
-        }
-
         
-//        cell.bookNumLabel.text = String("\(book)")
-//        cell.bookTitleLabel.text = String("\(book.title)")
-    
+        }
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
+
+//extension CodeViewController: UICollectionViewDelegateFlowLayout {
+//    private func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+//
+////           return CGSize(width: 250, height: 250)
+//
+//        return CGSize(width: collectionView.bounds.size.width/2 - 10, height: collectionView.bounds.size.height/2 - 10)
+//    }
+//}
+
---
+++ VYBORY/View/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  VYBORY
+//
+//  Created by Oksana Fedorchuk on 26.10.2020.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookNumber: UILabel!
+    @IBOutlet weak var bookName: UILabel!
+    
+}
