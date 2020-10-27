0222b235a57b1cd03bace2756ca4e533e0558f37
Oksana <klassi4eske@gmail.com>
2020-10-27 09:05:51 +0200

    Chapter entity created

--- VYBORY/Model/ChapterEntity.swift
+++ VYBORY/Model/ChapterEntity.swift
@@ -9,10 +9,30 @@ import Foundation
 import SQLite
 
 class ChaptersEntity {
+    
     static let shared = ChaptersEntity()
     
-    let chapters = Table("chapters")
+    let tblChapters = Table("chapters")
+    
     let number = Expression<String>("number")
     let title = Expression<String>("title")
     let bookNumber = Expression<String>("book_number")
+    
+    init() {
+        do {
+            if let connection = Database.shared.connection {
+                try connection.run(tblChapters.create(temporary: false, ifNotExists: true, withoutRowid: true, block: { (table) in
+                    table.column(self.number)
+                    table.column(self.title)
+                    table.column(self.bookNumber)
+                }))
+                print ("Table Chapters has been created")
+            } else {
+                print ("Fail creating the table Chapters")
+            }
+        }catch {
+            let nserror = error as NSError
+            print("Fail creating the table Chapters. Error: \(nserror)")
+        }
+    }
 }
